879ea6becdd37a3eb68bcc264ec3584ef8dd0062
julian <juliyachedev@gmail.com>
2022-10-19 12:07:41 -0300

    origin

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ export class Server extends ConfigServer {
     this.app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
     this.app.use(morgan('dev'))
-    this.app.use(cors({ credentials: true, origin: 'https://incredible-brigadeiros-a41bd6.netlify.app' }))
+    this.app.use(cors({ credentials: true, origin: true }))
     this.app.set('trust proxy', 1)
 
     this.app.use(this.routers())
